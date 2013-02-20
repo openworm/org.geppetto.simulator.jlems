9ded6fb431d69856469a337e64daa5ebf896e84a
Matteo Cantarelli <matteo.cantarelli@gmail.com>
2013-02-20 10:06:36 +0000

    Skeleton

---
+++ src/main/java/org/geppetto/solver/jlems/JLEMSSolverService.java
@@ -0,0 +1,29 @@
+/**
+ * 
+ */
+package org.geppetto.solver.jlems;
+
+import java.util.List;
+
+import org.openworm.simulationengine.core.model.IModel;
+import org.openworm.simulationengine.core.simulation.ITimeConfiguration;
+import org.openworm.simulationengine.core.solver.ISolver;
+
+/**
+ * @author matteocantarelli
+ *
+ */
+public class JLEMSSolverService implements ISolver
+{
+
+	/* (non-Javadoc)
+	 * @see org.openworm.simulationengine.core.solver.ISolver#solve(java.util.List, org.openworm.simulationengine.core.simulation.ITimeConfiguration)
+	 */
+	@Override
+	public List<List<IModel>> solve(List<IModel> models, ITimeConfiguration timeConfiguration)
+	{
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
